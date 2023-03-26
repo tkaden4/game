b51d07c906cef9f05e53aec1cb1ab6ab4d771bde
tkaden4 <thomas.kaden4@outlook.com>
2023-03-26 16:10:07 -0700

    Update dev script

--- scripts/assets.mjs
+++ scripts/assets.mjs
@@ -1,22 +1,48 @@
 import { writeFileSync } from "fs";
 import glob from "glob";
 import path from "path";
+import fs from "node:fs"
 
-let sfx = [];
-for (const font of glob.sync("./assets/sfx/fonts/*")) {
-  sfx.push({
-    path: font.replaceAll("\\", "/"),
-    name: path.basename(font, path.extname(font))
-  });
+function createMetadata() {
+  let sfx = [];
+  for (const font of glob.sync("./assets/sfx/fonts/*")) {
+    sfx.push({
+      path: font.replaceAll("\\", "/"),
+      name: path.basename(font, path.extname(font))
+    });
+  }
+
+  console.log(sfx.length, "sound files");
+
+  let gfx = [];
+  for (const image of glob.sync("./assets/gfx/*")) {
+    gfx.push({
+      path: image.replaceAll("\\", "/"),
+      name: path.basename(image, path.extname(image))
+    });
+  }
+
+  console.log(gfx.length, "graphics files");
+
+  writeFileSync("./assets/meta/sfx.json", JSON.stringify(sfx, null, 2));
+  writeFileSync("./assets/meta/gfx.json", JSON.stringify(gfx, null, 2));
 }
 
-let gfx = [];
-for (const image of glob.sync("./assets/gfx/*")) {
-  gfx.push({
-    path: image.replaceAll("\\", "/"),
-    name: path.basename(image, path.extname(image))
-  });
-}
 
-writeFileSync("./assets/meta/sfx.json", JSON.stringify(sfx, null, 2));
-writeFileSync("./assets/meta/gfx.json", JSON.stringify(gfx, null, 2));
+if (process.argv.includes("--watch")) {
+  createMetadata();
+  const gfxWatch = fs.watch("assets/gfx", { recursive: true });
+  const sfxwatch = fs.watch("assets/sfx", { recursive: true });
+
+
+  gfxWatch.on("change", (e, s) => {
+    console.log(e, s);
+    createMetadata();
+  })
+  sfxwatch.on("change", (e, s) => {
+    console.log(e, s);
+    createMetadata();
+  })
+} else {
+  createMetadata();
+}
